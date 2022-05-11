4661d91afb74acb1f2c27b116947871027f54381
Gerard Viñuela <gerardviru@gmail.com>
2022-05-11 12:38:20 +0200

    Proyecto Gradle String Framework UD23

---
+++ ProyectoEjemplo-1/src/main/java/com/example/demo/ProyectoEjemplo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEjemplo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEjemplo1Application.class, args);
+	}
+
+}
